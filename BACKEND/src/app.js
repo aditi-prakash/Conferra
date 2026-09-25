11671215fa3233b31b63393b3aea58cc53187252
Aditi Prakash <aditiprakash0338@gmail.com>
2026-09-25 11:52:12 +0530

    CORS error fix

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -44,7 +44,6 @@ const io = connectToSocket(server, allowedOrigins);
 app.set("port", process.env.PORT || 5000);
 
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
@@ -73,4 +72,4 @@ const start = async () => {
   });
 };
 
-start();
+start();
